d53b0c1ed15ff8d762252b705772e1fdd74bc997
JungCode <buidinhtrung0704@gmail.com>
2023-11-26 16:52:35 +0700

    first commit

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "Yay, see you on the 18th!!!!!!!!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
+const noBtnRect = noBtn.getBoundingClientRect();
 noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
 
